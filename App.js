00b5e056e17ea14546c73ff659a3243ce9c661ff
meliat329 <meliatomlinson2022@u.northwestern.edu>
2021-04-05 08:22:36 -0500

    Melia's change

--- App.js
+++ App.js
@@ -3,23 +3,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Life Capsule</Text>
-      <Text>Zain's line</Text>
-      <Text>Si Woo's line</Text>
-      <Text> Ikenna's line</Text>
-      <Text>Darryl's line</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Life Capsule</Text>
+            <Text>Zain's line</Text>
+            <Text>Si Woo's line</Text>
+            <Text> Ikenna's line</Text>
+            <Text>Darryl's line</Text>
+            <Text>Melia's line</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });

e0df4072ac30f66547a73eff1e5b4f4cbf67850b
Ikenna Elue <jonathanelue@yahoo.com>
2021-04-04 22:24:07 -0500

    Ikenna First Commit

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default function App() {
       <Text>Life Capsule</Text>
       <Text>Zain's line</Text>
       <Text>Si Woo's line</Text>
+      <Text> Ikenna's line</Text>
       <StatusBar style="auto" />
     </View>
   );

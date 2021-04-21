a29e7a67049e696fe3431d5a3c0f1103ce19e5a2
siwoopark10 <57846202+siwoopark10@users.noreply.github.com>
2021-04-21 18:30:16 -0500

    imported fonts that work in android

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
 import { StyleSheet, Text, View, ScrollableList, TextInput, Button } from 'react-native';
 import { Weather, DayRating, Exercise, Wake, LogStart } from './questions/Questions';
 import { Charts } from './charts/Charts';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { useEffect } from 'react';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    Font.loadAsync({
+      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+      'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+      'roboto-thin': require('./assets/fonts/Roboto-Thin.ttf')
+    })
+  },[])
+
     return (
       <NavigationContainer>
         <Stack.Navigator>

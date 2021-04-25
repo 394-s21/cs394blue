4789d4d364e4abe77ab4665e3c34f4e88c23a600
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-04-25 15:12:30 -0500

    Add wakeup chart

--- charts/Charts.js
+++ charts/Charts.js
@@ -34,6 +34,7 @@ export function Charts({navigation}) {
         {WeatherChart(data)}
         {StreakChart(data)}
         {ProductivityChart(data)}
+        {WakingChart(data)}
         <TouchableOpacity onPress={()=>navigation.navigate('Home')} style={styles.button}>
                 <Text style={styles.buttonText}>Return to Home</Text>
         </TouchableOpacity>
@@ -351,6 +352,77 @@ function ProductivityChart(data) {
         withVerticalLines={false}
         width={Dimensions.get("window").width}
         height={250}
+        chartConfig={{
+          backgroundGradientFromOpacity: 0,
+          backgroundGradientToOpacity: 0,
+          decimalPlaces: 0,
+          color: () => '#ffffff',
+        }}
+      />
+    </View>
+  )
+}
+
+function WakingChart(data) {
+  if(!data.length) {
+    return;
+  }
+
+  // TODO: Make number of days be an input
+  const numDays = 7;
+  const endDate = new Date();
+  let startDate = new Date();
+  startDate.setDate(endDate.getDate() - numDays + 1);
+
+  let date = startDate;
+  // Keep track of [sum, count]
+  let values = {
+    "5:00 - 6:30 AM": 0,
+    "6:30 - 8:00 AM": 0,
+    "8:00 - 9:30 AM": 0,
+    "9:30 - 11:00 AM": 0,
+    "11:00 - 12:30 PM": 0,
+    "After 12:30 PM": 0
+  }
+
+  // Loop through entries by date and fetch data
+  for (let i = 0; i < numDays; i++) {
+    let entry = data.find(e => {
+      let entryDate = new Date(e.date.replace(/-/g, '/'))
+      entryDate.setHours(0, 0, 0, 0)
+      date.setHours(0, 0, 0, 0)
+      return entryDate.valueOf() === date.valueOf();
+    })
+    if (entry) {
+      values[entry.wakeupTime] += 1;
+    }
+
+    // Increment date
+    date.setDate(date.getDate() + 1);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>Wakeup Time</Text>
+      <Text style={styles.description}>Times that you woke up this week</Text>
+      <BarChart
+        data={{
+          labels: Object.keys(values),
+          datasets: [
+            {
+              // TODO: Find workaround for setting max y value
+              // If no values are recorded for the option return 0
+              data: Object.values(values)
+            }
+          ]
+        }}
+        fromZero={true}
+        // segments={Math.max(Object.values(values))}
+        segments={Object.values(values).reduce((a, b) => a > b ? a : b)}
+        // withInnerLines={false}
+        withVerticalLines={false}
+        width={Dimensions.get("window").width}
+        height={300}
         chartConfig={{
           backgroundGradientFromOpacity: 0,
           backgroundGradientToOpacity: 0,

151598966cc9dd8c5b16dd4464c56e5d13d4ba71
Banksy-Yin <60046203+Banksy-Yin@users.noreply.github.com>
2021-04-27 17:57:58 +0800

    fixed chart color discrepancy

--- charts/StreakChart.js
+++ charts/StreakChart.js
@@ -88,16 +88,13 @@ export default function StreakChart(data) {
         height={300}
         strokeWidth={40}
         radius={100}
-        chartConfig = {{
-          backgroundGradientFrom: "#1E2923",
-          backgroundGradientFromOpacity: 0,
-          backgroundGradientTo: "#08130D",
-          backgroundGradientToOpacity: 0.5,
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          strokeWidth: 3, // optional, default 3
-          barPercentage: 0.5,
-          useShadowColorFromDataset: false // optional
-        }}
+        chartConfig={{
+
+            backgroundGradientFromOpacity: 0,
+            backgroundGradientToOpacity: 0,
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          }}
         hideLegend={true}
       />
     </View>

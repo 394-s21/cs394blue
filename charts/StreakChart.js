3c2f769c34883612df3198807fd1eebccfd45f15
Banksy-Yin <60046203+Banksy-Yin@users.noreply.github.com>
2021-04-27 17:50:59 +0800

    charts refactored into multiple files

---
+++ charts/StreakChart.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { ProgressChart } from 'react-native-chart-kit';
+
+export default function StreakChart(data) {
+
+  if(!data.length) {
+    return;
+  }
+
+  // TODO: Make number of days be an input
+  const numDays = 7;
+  const endDate = new Date();
+  let startDate = new Date();
+  startDate.setDate(endDate.getDate() - numDays + 1);
+
+  let date = startDate;
+  // Set range values
+  let listDates = [
+    {date: "2000-01-01", count: 0},
+    {date: "2000-01-02", count: 1}
+  ]
+
+  //code for progress chart
+  var count = 0;
+
+  // Loop through entries by date and fetch data
+  for (let i = 0; i < numDays; i++) {
+    let entry = data.find(e => {
+      let entryDate = new Date(e.date.replace(/-/g, '/'))
+      entryDate.setHours(0, 0, 0, 0)
+      date.setHours(0, 0, 0, 0)
+      return entryDate.valueOf() === date.valueOf();
+    })
+    if (entry) {
+      listDates.push({date: entry.date, count: 1});
+      count++;
+    } else {
+
+    }
+
+    // Increment date
+    date.setDate(date.getDate() + 1);
+  }
+
+  const streakPercentage = count / numDays;
+  const progressData = {
+    labels: ["Streak"],
+    data: [streakPercentage]
+  };
+
+  const Days = () => {
+    return (<View style={styles.dayBox}>
+      {/* <Text>{date}</Text>
+      <Text>{day}</Text> */}
+    </View>)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>Your Streak</Text>
+      <Text style={styles.description}>The percentage of days you logged in the past week is lit up</Text>
+      {/* <View style={styles.daysContainer}>
+        <Days />
+        <Days />
+        <Days />
+        <Days />
+        <Days />
+        <Days />
+        <Days />
+      </View>
+      <ContributionGraph
+        values={listDates}
+        endDate={endDate}
+        numDays={numDays}
+        width={Dimensions.get("window").width}
+        height={200}
+        chartConfig={{
+          backgroundGradientFromOpacity: 0,
+          backgroundGradientToOpacity: 0,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: () => '#ffffff',
+        }}
+      /> */}
+      <ProgressChart
+        data={progressData}
+        width={Dimensions.get("window").width}
+        height={300}
+        strokeWidth={40}
+        radius={100}
+        chartConfig = {{
+          backgroundGradientFrom: "#1E2923",
+          backgroundGradientFromOpacity: 0,
+          backgroundGradientTo: "#08130D",
+          backgroundGradientToOpacity: 0.5,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          strokeWidth: 3, // optional, default 3
+          barPercentage: 0.5,
+          useShadowColorFromDataset: false // optional
+        }}
+        hideLegend={true}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0099cc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontFamily: 'roboto-regular'
+  },
+  dayBox: {
+    width: '10%',
+    height: 100,
+    backgroundColor: 'rgb(255,255,255)',
+    margin:20
+  },
+  daysContainer: {
+    width: '100%',
+    flexDirection:'row',
+    padding: 10
+  }
+});
---
+++ charts/WakingChart.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { BarChart } from 'react-native-chart-kit';
+
+
+export default function WakingChart(data) {
+  if(!data.length) {
+    return;
+  }
+
+  // TODO: Make number of days be an input
+  const numDays = 7;
+  const endDate = new Date();
+  let startDate = new Date();
+  startDate.setDate(endDate.getDate() - numDays + 1);
+
+  let date = startDate;
+  // Keep track of [sum, count]
+  let values = {
+    "5:00 - 6:30 AM": 0,
+    "6:30 - 8:00 AM": 0,
+    "8:00 - 9:30 AM": 0,
+    "9:30 - 11:00 AM": 0,
+    "11:00 - 12:30 PM": 0,
+    "After 12:30 PM": 0
+  }
+
+  // Loop through entries by date and fetch data
+  for (let i = 0; i < numDays; i++) {
+    let entry = data.find(e => {
+      let entryDate = new Date(e.date.replace(/-/g, '/'))
+      entryDate.setHours(0, 0, 0, 0)
+      date.setHours(0, 0, 0, 0)
+      return entryDate.valueOf() === date.valueOf();
+    })
+    if (entry) {
+      values[entry.wakeupTime] += 1;
+    }
+
+    // Increment date
+    date.setDate(date.getDate() + 1);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>Wakeup Time</Text>
+      <Text style={styles.description}>Times that you woke up this week</Text>
+      <BarChart
+        data={{
+          labels: Object.keys(values),
+          datasets: [
+            {
+              // TODO: Find workaround for setting max y value
+              // If no values are recorded for the option return 0
+              data: Object.values(values)
+            }
+          ]
+        }}
+        fromZero={true}
+        // segments={Math.max(Object.values(values))}
+        segments={Object.values(values).reduce((a, b) => a > b ? a : b)}
+        // withInnerLines={false}
+        withVerticalLines={false}
+        width={Dimensions.get("window").width}
+        height={300}
+        chartConfig={{
+          backgroundGradientFromOpacity: 0,
+          backgroundGradientToOpacity: 0,
+          decimalPlaces: 0,
+          color: () => '#ffffff',
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0099cc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontFamily: 'roboto-regular'
+  },
+});
